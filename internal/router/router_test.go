ba6ea1d50e811b6117d354e98b317cde617654b4
kingcobra2468 <eriktyryshkin@gmail.com>
2023-11-11 16:03:22 -0800

    Add stress test

--- internal/router/router_test.go
+++ internal/router/router_test.go
@@ -13,6 +13,7 @@ import (
 
 const commandName = "test"
 const recipientNumber = "1"
+const coolDown = time.Duration(time.Second * 3)
 
 func stopEventLoop(t *testing.T, done chan struct{}, timeout time.Duration) {
 	time.Sleep(time.Duration(timeout))
@@ -33,7 +34,7 @@ func TestProcess(t *testing.T) {
 	mockWorker.On("Recipient").Return(recipientNumber)
 	mockWorker.On("Send", mock.Anything).Return(nil)
 
-	el := NewEventLoop(2, 2, &cache)
+	el := NewEventLoop(2, 2, coolDown, &cache)
 	el.AddWorker(mockWorker)
 
 	done := make(chan struct{})
@@ -52,6 +53,46 @@ func TestProcess(t *testing.T) {
 	mockWorker.AssertCalled(t, "Send", mock.Anything)
 }
 
+func TestProcess_stress(t *testing.T) {
+	server := testutil.NewStubServer(t)
+	s := testutil.NewFakeService(server, commandName, recipientNumber)
+
+	cache := *service.NewCache()
+	cache.Add(*s)
+
+	mockWorkers := make([]*mocks.Worker, 8)
+	el := NewEventLoop(8, 6, coolDown, &cache)
+
+	for i := 0; i < 8; i++ {
+		mockWorker := mocks.NewWorker(t)
+		mockWorker.EXPECT().Fetch().Return(&[]service.UserInput{{Name: commandName, Raw: "test"}})
+		mockWorker.On("LoopBack").Return(false)
+		mockWorker.On("Recipient").Return(recipientNumber)
+		mockWorker.On("Send", mock.Anything).Return(nil)
+
+		//mockWorkers = append(mockWorkers, mockWorker)
+		mockWorkers[i] = mockWorker
+		el.AddWorker(mockWorker)
+	}
+
+	done := make(chan struct{})
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+
+	go stopEventLoop(t, done, time.Second*20)
+
+	t.Log("started event loop")
+	el.Start(done, &wg)
+
+	wg.Wait()
+
+	for _, mockWorker := range mockWorkers {
+		mockWorker.AssertExpectations(t)
+		mockWorker.AssertCalled(t, "Fetch", mock.Anything)
+		mockWorker.AssertCalled(t, "Send", mock.Anything)
+	}
+}
+
 func TestProcess_ping(t *testing.T) {
 	server := testutil.NewStubServer(t)
 	s := testutil.NewFakeService(server, commandName, recipientNumber)
@@ -64,7 +105,7 @@ func TestProcess_ping(t *testing.T) {
 	mockWorker.EXPECT().Send("pong").Return(nil)
 	mockWorker.On("Send", mock.Anything).Return(nil)
 
-	el := NewEventLoop(2, 2, &cache)
+	el := NewEventLoop(2, 2, coolDown, &cache)
 	el.AddWorker(mockWorker)
 
 	done := make(chan struct{})
@@ -92,7 +133,7 @@ func TestProcess_pong(t *testing.T) {
 	mockWorker.EXPECT().Fetch().Return(&[]service.UserInput{{Name: "pong", Raw: "pong"}})
 	mockWorker.On("LoopBack").Return(true)
 
-	el := NewEventLoop(2, 2, &cache)
+	el := NewEventLoop(2, 2, coolDown, &cache)
 	el.AddWorker(mockWorker)
 
 	done := make(chan struct{})
